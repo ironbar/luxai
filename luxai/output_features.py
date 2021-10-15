62f9c2487ca3c4da7aa921501fe2b7c2a2a863f0
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-15 18:18:58 +0200

    created city actions

--- luxai/output_features.py
+++ luxai/output_features.py
@@ -11,3 +11,27 @@ def create_actions_mask(active_units_to_position, observation):
         x, y = position
         mask[x, y] = 1
     return mask
+
+UNIT_ACTIONS_MAP = {
+
+}
+
+
+CITY_ACTIONS_MAP = {
+    'r': 0, # research
+    'bw': 1, # build worker
+    'bc': 2, # build cart
+}
+
+
+def create_output_features(actions, units_to_position, observation):
+    width, height = observation['width'], observation['height']
+
+    city_actions = np.zeros((len(CITY_ACTIONS_MAP), width, height), dtype=np.float32)
+    for action in actions:
+        splits = action.split(' ')
+        action_id = splits[0]
+        if action_id in CITY_ACTIONS_MAP:
+            x, y = int(splits[1]), int(splits[2])
+            city_actions[CITY_ACTIONS_MAP[action_id], x, y] = 1
+    return city_actions
