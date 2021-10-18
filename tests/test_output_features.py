ebb5a72cb64457bd3e88a3ceb7b60a2d66da80f4
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-18 17:02:13 +0200

    Added tests for output features

---
+++ tests/test_output_features.py
@@ -0,0 +1,36 @@
+import pytest
+import json
+import os
+
+from luxai.input_features import make_input
+from luxai.output_features import create_actions_mask, create_output_features
+
+# TODO: once I'm able to generate actions from predictions I can test that by using perfect labels I can
+# regenerate the actions for a match
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.mark.parametrize('filepath', [
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210924_seed1_172steps.json'),
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210923_seed0_240steps.json'),
+])
+def test_that_there_are_no_actions_outside_the_masks(filepath):
+    with open(filepath, 'r') as f:
+        match = json.load(f)
+
+    for step in range(len(match['steps']) - 1):
+        observation = match['steps'][step][0]['observation']
+        actions = match['steps'][step+1][0]['action'] # notice the step + 150
+
+        ret = make_input(observation)
+        active_units_to_position, active_cities_to_position, units_to_position = ret[2:]
+        unit_actions_mask = create_actions_mask(active_units_to_position, observation)
+        city_actions_mask = create_actions_mask(active_cities_to_position, observation)
+        unit_actions, city_actions = create_output_features(actions, units_to_position, observation)
+
+        _assert_that_there_are_no_actions_outside_the_mask(city_actions, city_actions_mask)
+        _assert_that_there_are_no_actions_outside_the_mask(unit_actions, unit_actions_mask)
+
+
+def _assert_that_there_are_no_actions_outside_the_mask(actions, mask):
+    pass
