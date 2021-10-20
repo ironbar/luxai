442c5daaa073ffff8a7a7b6ab1e1fcd95986094f
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-20 17:30:34 +0200

    improved documentation

--- luxai/actions.py
+++ luxai/actions.py
@@ -7,6 +7,18 @@ from luxai.output_features import CITY_ACTIONS_MAP
 
 
 def create_actions_for_cities_from_model_predictions(preds, active_cities_to_position, action_threshold=0.5):
+    """
+    Creates actions in the luxai format from the predictions of the model
+
+    Parameters
+    ----------
+    preds : 3d array
+        3d array with the predictions being the dimensions (x, y, action)
+    active_cities_to_position : dict
+        A dictionary that maps city tile identifier to x, y position
+    action_threshold : float
+        A threshold that filters predictions with a smaller value than that
+    """
     actions = []
     idx_to_action = {idx: name for name, idx in CITY_ACTIONS_MAP.items()}
     for position in active_cities_to_position.values():
