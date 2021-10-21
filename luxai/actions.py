82ae95c54136f769dc47cb3c3314b06a17096eb0
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-21 15:55:41 +0200

    Added function to create unit actions from predictions

--- luxai/actions.py
+++ luxai/actions.py
@@ -3,7 +3,7 @@ Functions for generating actions from model predictions
 """
 import numpy as np
 
-from luxai.output_features import CITY_ACTIONS_MAP
+from luxai.output_features import CITY_ACTIONS_MAP, UNIT_ACTIONS_MAP
 
 
 def create_actions_for_cities_from_model_predictions(preds, active_cities_to_position, action_threshold=0.5):
@@ -29,3 +29,48 @@ def create_actions_for_cities_from_model_predictions(preds, active_cities_to_pos
             action_key = idx_to_action[action_idx]
             actions.append('%s %i %i' % (action_key, x, y))
     return actions
+
+
+def create_actions_for_units_from_model_predictions(preds, active_units_to_position, units_to_position, action_threshold=0.5):
+    """
+    Creates actions in the luxai format from the predictions of the model
+
+    Parameters
+    ----------
+    preds : 3d array
+        3d array with the predictions being the dimensions (x, y, action)
+    active_units_to_position : dict
+        A dictionary that maps active unit identifier to x,y position
+    units_to_position : dict
+        A dictionary that maps all unit identifier to x,y position
+    action_threshold : float
+        A threshold that filters predictions with a smaller value than that
+    """
+    actions = []
+    idx_to_action = {idx: name for name, idx in UNIT_ACTIONS_MAP.items()}
+    for unit_id, position in active_units_to_position.items():
+        x, y = position
+        unit_preds = preds[x, y]
+        # TODO: be able to deal with multiple units on same place
+        action_idx = np.argmax(unit_preds)
+        if unit_preds[action_idx] > action_threshold:
+            action_key = idx_to_action[action_idx]
+            actions.append(create_unit_action(action_key, unit_id))
+    # TODO: deal with collisions
+    return actions
+
+
+def create_unit_action(action_key, unit_id):
+    action_id = action_key.split(' ')[0]
+    if action_id == 'm':
+        action = 'm %s %s' % (unit_id, action_key.split(' ')[-1])
+        return action
+    elif action_id in ['bcity', 'p']:
+        action = '%s %s' % (action_id, unit_id)
+        return action
+    elif action_id == 't':
+        # TODO: implement transfer
+        return 't %s' % unit_id
+        # return 'm %s c' % unit_id
+    else:
+        raise KeyError(action_id)
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,7 +2,9 @@ import pytest
 import os
 import json
 
-from luxai.actions import create_actions_for_cities_from_model_predictions, CITY_ACTIONS_MAP
+from luxai.actions import (
+    create_actions_for_cities_from_model_predictions, CITY_ACTIONS_MAP,
+    create_actions_for_units_from_model_predictions, UNIT_ACTIONS_MAP)
 from luxai.input_features import make_input
 from luxai.output_features import create_output_features
 
@@ -37,3 +39,61 @@ def test_city_actions_can_be_recovered_from_ground_truth(filepath, player):
             assert all(action in actions for action in recovered_actions), msg
         if true_city_actions:
             assert all(action in recovered_actions for action in true_city_actions), msg
+
+
+@pytest.mark.parametrize('player', range(2))
+@pytest.mark.parametrize('filepath', [
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210924_seed1_172steps.json'),
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210923_seed0_240steps.json'),
+])
+def test_unit_actions_can_be_recovered_from_ground_truth_if_no_unit_overlaps_and_skipping_transfer(filepath, player):
+    with open(filepath, 'r') as f:
+        match = json.load(f)['steps']
+    for step in range(len(match) - 1):
+        observation = match[step][0]['observation']
+        if player:
+            observation.update(match[step][player]['observation'])
+        actions = match[step+1][player]['action'] # notice the step + 1
+        if actions is None: # this can happen on timeout
+            continue
+
+        ret = make_input(observation)
+        active_units_to_position, active_cities_to_position, units_to_position = ret[2:]
+        unit_actions_ground_truth, _ = create_output_features(actions, units_to_position, observation)
+
+        recovered_actions = create_actions_for_units_from_model_predictions(
+            unit_actions_ground_truth, active_units_to_position, units_to_position)
+
+        units_with_overlap = _get_units_with_overlap(units_to_position)
+        true_unit_actions = _remove_actions_with_overlap_or_transfer(actions, units_with_overlap)
+        recovered_actions = _remove_actions_with_overlap_or_transfer(recovered_actions, units_with_overlap)
+
+        msg = 'actions:  %s\ntrue unit actions:      %s\nrecovered unit actions: %s' % (
+            sorted(actions), sorted(true_unit_actions), sorted(recovered_actions))
+        msg += '\nunits with overlap: %s' % units_with_overlap
+        if recovered_actions:
+            assert all(action in actions for action in recovered_actions), msg
+        if true_unit_actions:
+            assert all(action in recovered_actions for action in true_unit_actions), msg
+
+
+def _remove_actions_with_overlap_or_transfer(actions, units_with_overlap):
+    true_unit_actions = [action for action in actions if action.split(' ')[0] in set(['m', 'bcity', 'p'])]
+    true_unit_actions = [action for action in true_unit_actions if not action.endswith(' c')]
+    true_unit_actions = [action for action in true_unit_actions if not any(_is_action_from_unit(action, unit_id) for unit_id in units_with_overlap)]
+    return true_unit_actions
+
+
+def _get_units_with_overlap(units_to_position):
+    units_with_overlap = []
+    for unit_id, position in units_to_position.items():
+        for other_unit_id, other_position in units_to_position.items():
+            if unit_id == other_unit_id:
+                continue
+            if position == other_position:
+                units_with_overlap.append(unit_id)
+    return units_with_overlap
+
+
+def _is_action_from_unit(action, unit_id):
+    return ' %s ' % unit_id in action or action.endswith(unit_id)
