b4cb1553447def9688a580eb4e9d7d9e1f2b26b6
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-21 16:32:57 +0200

    Added test for transfer actions

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -123,3 +123,37 @@ def _remove_actions_without_overlap_or_with_transfer(actions, units_with_overlap
     true_unit_actions = [action for action in true_unit_actions if not action.endswith(' c')]
     true_unit_actions = [action for action in true_unit_actions if any(_is_action_from_unit(action, unit_id) for unit_id in units_with_overlap)]
     return true_unit_actions
+
+
+@pytest.mark.parametrize('player', [0])
+@pytest.mark.parametrize('filepath', [
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210924_seed1_172steps.json'),
+    os.path.join(SCRIPT_DIR, '../data/sample_games/20210923_seed0_240steps.json'),
+])
+def test_transfer_actions_can_be_recovered_from_ground_truth_if_no_unit_overlaps(filepath, player):
+    with open(filepath, 'r') as f:
+        match = json.load(f)['steps']
+    for observation, actions in step_generator(match, player):
+        active_units_to_position, _, units_to_position = make_input(observation)[2:]
+        unit_actions_ground_truth, _ = create_output_features(actions, units_to_position, observation)
+
+        recovered_actions = create_actions_for_units_from_model_predictions(
+            unit_actions_ground_truth, active_units_to_position, units_to_position)
+
+        units_with_overlap = _get_units_with_overlap(units_to_position)
+        true_unit_actions = _remove_actions_with_overlap_or_without_transfer(actions, units_with_overlap)
+        recovered_actions = _remove_actions_with_overlap_or_without_transfer(recovered_actions, units_with_overlap)
+
+        msg = 'actions:  %s\ntrue unit actions:      %s\nrecovered unit actions: %s' % (
+            sorted(actions), sorted(true_unit_actions), sorted(recovered_actions))
+        msg += '\nunits with overlap: %s' % units_with_overlap
+        if recovered_actions:
+            assert all(action in actions for action in recovered_actions), msg
+        if true_unit_actions:
+            assert all(action in recovered_actions for action in true_unit_actions), msg
+
+
+def _remove_actions_with_overlap_or_without_transfer(actions, units_with_overlap):
+    true_unit_actions = [action for action in actions if action.split(' ')[0]== 't']
+    true_unit_actions = [action for action in true_unit_actions if not any(_is_action_from_unit(action, unit_id) for unit_id in units_with_overlap)]
+    return true_unit_actions
