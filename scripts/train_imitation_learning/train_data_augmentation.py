fbde88e82a3185a8d87218b07f6bce090ea04956
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-27 14:59:52 +0200

    Added script to train with data augmentation

---
+++ scripts/train_imitation_learning/train_data_augmentation.py
@@ -0,0 +1,62 @@
+import sys
+import os
+import yaml
+import argparse
+import numpy as np
+
+import tensorflow as tf
+
+from luxai.data import load_train_and_test_data
+from luxai.data_augmentation import random_data_augmentation
+
+from train import create_model, create_callbacks
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    train(args.config_path)
+
+
+def train(config_path):
+    with open(config_path) as f:
+        train_conf = yaml.safe_load(f)
+    output_folder = os.path.dirname(os.path.realpath(config_path))
+
+    train_data, test_data = load_train_and_test_data(**train_conf['data'])
+    model = create_model(train_conf['model_params'])
+    callbacks = create_callbacks(train_conf['callbacks'], output_folder)
+    model.fit(x=train_generator(train_data, train_conf['batch_size']), validation_data=test_data,
+              callbacks=callbacks, **train_conf['train_kwargs'])
+
+
+def train_generator(train_data, batch_size):
+    indices = np.arange(len(train_data[0][0]))
+    while 1:
+        np.random.shuffle(indices)
+        n_batches = len(indices)//batch_size
+        for batch_idx in range(n_batches):
+            batch_indices = indices[batch_idx*batch_size: (batch_idx+1)*batch_size]
+            x = train_data[0][0][batch_indices], train_data[0][1][batch_indices]
+            y = train_data[1][0][batch_indices], train_data[1][1][batch_indices]
+            yield random_data_augmentation(x, y)
+
+
+def parse_args(args):
+    epilog = """
+    python train.py /mnt/hdd0/Kaggle/luxai/models/01_first_steps/train_conf.yml
+    """
+    description = """
+    Train model for imitation learning with data augmentation
+    """
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog=epilog)
+    parser.add_argument('config_path', help='Path to the yaml file with train configuration')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
