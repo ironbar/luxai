d43b6c8dbd4ed3b741a276b813ce5581cd9508d9
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2021-10-30 09:38:01 +0200

    updated create_cunet_agent script to support ensembles

--- scripts/create_cunet_agent/create_cunet_agent.py
+++ scripts/create_cunet_agent/create_cunet_agent.py
@@ -11,15 +11,16 @@ def main(args=None):
     if args is None:
         args = sys.argv[1:]
     args = parse_args(args)
-    create_cunet_agent(args.model_path, args.output_path)
+    create_cunet_agent(args.model_paths, args.output_path)
 
 
-def create_cunet_agent(model_path, output_path):
+def create_cunet_agent(model_paths, output_path):
     os.makedirs(output_path)
     clone_cunet_repo(output_path)
-    shutil.copyfile(model_path, os.path.join(output_path, 'model.h5'))
+    for idx, model_path in enumerate(model_paths):
+        shutil.copyfile(model_path, os.path.join(output_path, 'model_%i.h5' % idx))
     download_luxai_sample_agent(output_path)
-    create_cunet_agent_script(model_path, output_path)
+    create_cunet_agent_script(model_paths, output_path)
     shutil.copyfile('template_main.py', os.path.join(output_path, 'main.py'))
 
 
@@ -37,7 +38,7 @@ def download_luxai_sample_agent(output_path):
     os.system(cmd)
 
 
-def create_cunet_agent_script(model_path, output_path):
+def create_cunet_agent_script(model_paths, output_path):
     module_paths = [
         '../../luxai/input_features.py',
         '../../luxai/output_features.py',
@@ -50,15 +51,16 @@ def create_cunet_agent_script(model_path, output_path):
             text += f.read() + '\n'*3
     text = text.replace('kaggle_environments.envs.lux_ai_2021.test_agents.python.', '')
     text = text.replace('from luxai', '#from luxai')
-    text = text.replace('__replace_model_path__', os.path.realpath(os.path.join(output_path, 'model.h5')))
-    text = text.replace('__replace_original_model_path__', model_path)
+    text = text.replace('__replace_original_model_path__', str(model_paths))
     with open(os.path.join(output_path, 'agent.py'), 'w') as f:
         f.write(text)
 
 
 def parse_args(args):
     epilog = """
-    python create_cunet_agent.py /mnt/hdd0/Kaggle/luxai/models/09_even_more_architecture_variations_around_condition/02_filters32_depth4_condition_8_complex/best_val_loss_model.h5 ../../agents/clown
+    python create_cunet_agent.py ../../agents/superfocus_64 /mnt/hdd0/Kaggle/luxai/models/19_ensemble/01_64_filters_rank0_pretrained_loss_weights_1_01_seed1/best_val_loss_model.h5
+
+    python create_cunet_agent.py ../../agents/superfocus_64_ensemble /mnt/hdd0/Kaggle/luxai/models/19_ensemble/*/best_val_loss_model.h5
     """
     description = """
     Creates an agent that uses a cunet model for playing
@@ -67,8 +69,8 @@ def parse_args(args):
         description=description,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog=epilog)
-    parser.add_argument('model_path', help='Path to .h5 cunet model file')
     parser.add_argument('output_path', help='Path to output folder where the agent will be created')
+    parser.add_argument('model_paths', help='Path to .h5 cunet model file, it could be single model or multiple ones', nargs='*')
     return parser.parse_args(args)
 
 
--- scripts/create_cunet_agent/template_agent.py
+++ scripts/create_cunet_agent/template_agent.py
@@ -2,27 +2,25 @@
 Agent code for cunet model
 """
 import os
+import glob
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 import tensorflow as tf
 import numpy as np
 import cunet.train.models.FiLM_utils
 
-try:
-    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-    model_path = os.path.join(SCRIPT_DIR, 'model.h5')
-except NameError:
-    # this happens when using python for playing matches
-    # original model path: __replace_original_model_path__
-    model_path = '__replace_model_path__'
-model = tf.keras.models.load_model(model_path, compile=False)
+# original model paths: __replace_original_model_path__
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+model_paths = sorted(glob.glob(os.path.join(SCRIPT_DIR, '*.h5')))
+models = [tf.keras.models.load_model(model_path, compile=False) for model_path in model_paths]
 
 
 def agent(observation, configuration):
     ret = make_input(observation)
     board, features = ret[:2]
-    preds = model.predict([
-        expand_board_size_adding_zeros(np.expand_dims(board, axis=0)),
-        np.expand_dims(features, axis=0)])
+    model_input = [expand_board_size_adding_zeros(np.expand_dims(board, axis=0)),
+                   np.expand_dims(features, axis=0)]
+    preds = [model.predict(model_input) for model in models]
+    preds = [np.sum([pred[idx] for pred in preds], axis=0) for idx in range(2)]
     preds = [crop_board_to_original_size(pred, observation) for pred in preds]
     active_unit_to_position, active_city_to_position, unit_to_position, city_to_position = ret[2:]
     actions = create_actions_for_units_from_model_predictions(
